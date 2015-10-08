4c30e4ed9f557833f8a4312777cc5e99b743d6c3
dsab123 <dsabbaghumd@gmail.com>
2015-10-08 18:45:31 -0400

    fixed js

--- js/ui.js
+++ js/ui.js
@@ -1,43 +1,48 @@
 $(document).ready(function() {
 	// this is gonne be the most complicated function...
 	// if hymn1 clicked, do a fade in from top
-	$('li[class^="list"]').on("click", function() {
-		var whichActive = $(this).parent().find(".active");
-		
-		whichActive.removeClass("active");
-		$(this).addClass("active");
-		
-		if ($(this).is(whichActive)) {
-			unselectHymn();
+	$('.list-hymn1').on("click", function() {
+
+		$('#hymn-info1').toggleClass("active");
+
+		if ($('#hymn-info1').hasClass("active")) {
+			$('#hymn-info1').fadeIn("fast");
+			$('#hymn-info2').fadeOut("fast");
+			$('#hymn-info2').removeClass("active");
+			$('#hymn-info3').fadeOut("fast");
+			$('#hymn-info3').removeClass("active");
 		} else {
-			$('#hymn-info').fadeIn("fast");
+			$('#hymn-info1').fadeOut("fast");
 		}
-	});	
-	
-	$('.list-hymn3', '.row3').on("click"), function() {
-		alert("hello");
-	});
-	
-	function selectHymn(text) {
-		alert("hello");
-	}
-	
-	function unselectHymn() {
-		$('.active').removeClass('active');
-		$('#hymn-info').fadeOut("fast");
-	}
 
-	$(function() {
-		$( ".datepicker" ).datepicker({
-			showOtherMonths: true,
-			selectOtherMonths: true,
-			beforeShowDay: enableSUNDAYS
-		});
+
 	});
 
-	function enableSUNDAYS(date) {
-		var day = date.getDay();
-		return [(day == 0), ''];
-	}
+
+	$('.list-hymn3').on("click", function() {
+		alert("hello");
+	});
+
+function selectHymn(text) {
+	alert("hello");
+}
+
+function unselectHymn() {
+	$('.active').removeClass('active');
+	$('#hymn-info').fadeOut("fast");
+}
+
+$(function() {
+	$( ".datepicker" ).datepicker({
+		showOtherMonths: true,
+		selectOtherMonths: true,
+		beforeShowDay: enableSUNDAYS
+	});
+});
+
+function enableSUNDAYS(date) {
+	var day = date.getDay();
+	return [(day == 0), ''];
+}
 
 });
